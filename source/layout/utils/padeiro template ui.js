aaaaa274c76264f18b84de0c9a53283fe7c1c842
jmbillard <jmbillard2@gmail.com>
2024-01-12 17:46:17 -0300

    -

--- source/layout/utils/padeiro template ui.js
+++ source/layout/utils/padeiro template ui.js
@@ -67,11 +67,35 @@ if (!Array.prototype.indexOf) {
 	};
 }
 
+//-------------------------------------------------------------
+
 // string to camel case...
 String.prototype.toTitleCase = function () {
 	return this.toLowerCase()
 		.replace(/\s(.)/g, function ($1) { return $1.toUpperCase(); })
 		.replace(/^(.)/, function ($1) { return $1.toUpperCase(); });
+};
+
+// replaces most of the special characters...
+String.prototype.replaceSpecialCharacters = function () {
+
+	return this.replace(/À|Á|Â|Ã|Ä|\u00C0|\u00C1|\u00C2|\u00C3|\u00C4/g, 'A')
+		.replace(/à|á|â|ã|ä|\u00E0|\u00E1|\u00E2|\u00E3|\u00E4/g, 'a')
+		.replace(/È|É|Ê|Ë|\u00C8|\u00C9|\u00CA|\u00CB/g, 'E')
+		.replace(/è|é|ê|ë|&|\u00E8|\u00E9|\u00EA|\u00EB/g, 'e')
+		.replace(/Ì|Í|Î|Ï|\u00CC|\u00CD|\u00CE|\u00CF/g, 'I')
+		.replace(/ì|í|î|ï|\u00EC|\u00ED|\u00EE|\u00EF/g, 'i')
+		.replace(/Ò|Ó|Ô|Õ|Ö|\u00D2|\u00D3|\u00D4|\u00D5|\u00D6/g, 'O')
+		.replace(/ò|ó|ô|õ|ö|\u00F2|\u00F3|\u00F4|\u00F5|\u00F6/g, 'o')
+		.replace(/Ù|Ú|Û|Ü|\u00D9|\u00DA|\u00DB|\u00DC/g, 'U')
+		.replace(/ù|ú|û|ü|\u00F9|\u00FA|\u00FB|\u00FC/g, 'u')
+		.replace(/Ç|\u00C7/g, 'C')
+		.replace(/ç|\u00E7/g, 'c')
+		.replace(/\n|\r/g, ' ') // replaces line breaks...
+		.replace(/\||-|\_|:/g, ' ')
+		.replace(/\s{2,}/g, ' ') // replaces 2 or more spaces...
+		.replace(/[^\w\s—]/ig, '') // replaces any non-word character except space...
+		.trim();
 };
 
 //-------------------------------------------------------------
@@ -436,15 +460,6 @@ function padeiroTemplateDialog() {
 		hasData = (edtText.text.trim() != '');
 		makeBtn.enabled = (templateTree.selection != null && hasData);
 	};
-
-	//---------------------------------------------------------
-
-	// edtText.blur = function () {
-
-	// 	hasData = (edtText.text.trim() != '');
-	// 	makeBtn.enabled = (templateTree.selection != null && hasData);
-	// 	if (!hasData) edtText.text = exemple;
-	// };
 
 	//---------------------------------------------------------
 
