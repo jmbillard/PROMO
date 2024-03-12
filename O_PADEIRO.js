f1dff9e044ad48cceaf2716f2ecc0914f278397c
jmbillard <jmbillard2@gmail.com>
2024-03-12 07:33:56 -0300

    -

--- O_PADEIRO.js
+++ O_PADEIRO.js
@@ -54,9 +54,8 @@ function O_PADEIRO_UTL(thisObj) {
 
 		PAD_w.layout.layout(true);
 
+		setTxtHighlight(PAD_vLab, '#FFFFFF','#FF7B79');
 		setBgColor(PAD_w, '#515D9E');
-
-		setTxtHighlight(PAD_vLab, '#FFFFFF', '#FF7B79');
 
 		PAD_w.onShow = PAD_w.onResizing = function () {
 			PAD_w.layout.layout(true);
