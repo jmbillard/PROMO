afc579a93312f46a26b212d20771a4b5db8fa0d5
jmbillard <jmbillard2@gmail.com>
2024-07-04 16:12:40 -0300

    -

---
+++ release/include/O PADEIRO PROD DEV.jsx
@@ -0,0 +1,22 @@
+/*
+
+---------------------------- info ----------------------------
+
+  title:   PROMO utilities script
+
+  notes:   a collection of tools designed to
+  speedup the motion graphics team workflow
+
+  author:  Jean-Marc Billard
+  version: xxx
+  date:    xx-xx-2023
+
+--------------------------------------------------------------
+
+*/
+//  jshint -W061
+try {
+  #include '~/PROMO/O_PADEIRO_PROD.js'; // → UI definition file
+} catch (err) {
+  alert('nope... (っ °Д °;)っ\n\n' + err.message);
+}
---
+++ release/include/O PADEIRO PROD.jsx
@@ -0,0 +1,28 @@
+/*
+
+---------------------------- info ----------------------------
+
+  title:   O PADEIRO script
+
+  notes:   a collection of templates and companion script
+
+  author:  Jean-Marc Billard
+  version: xxx
+  date:    xx-xx-2024
+
+--------------------------------------------------------------
+
+*/
+//  jshint -W061
+
+try {
+  var scriptFile = new File('//10.228.183.165/VFX/imagem/drive_l/Fileserver_3/INSTITUCIONAL/_adm-designers/SCRIPTS FREE/O_PADEIRO/O_PADEIRO_PROD.jsxbin');
+
+  scriptFile.open('r');
+  eval(scriptFile.read());
+
+  scriptFile.close();
+
+} catch (err) {
+  alert('nope... (っ °Д °;)っ\n\n' + err.message);
+}
