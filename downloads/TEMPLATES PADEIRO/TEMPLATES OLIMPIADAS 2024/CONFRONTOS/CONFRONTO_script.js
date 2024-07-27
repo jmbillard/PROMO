899c1d1c007ebe0b43576541aa4a2264b5d1a51c
jmbillard <jmbillard2@gmail.com>
2024-07-27 10:43:14 -0300

    -

--- downloads/TEMPLATES PADEIRO/TEMPLATES OLIMPIADAS 2024/CONFRONTOS/CONFRONTO_script.js
+++ downloads/TEMPLATES PADEIRO/TEMPLATES OLIMPIADAS 2024/CONFRONTOS/CONFRONTO_script.js
@@ -10,3 +10,18 @@ for (var t = 0; t < createdTemplatesArray.length; t++) {
 	} catch (err) { }
 }
 renamePromoComps(createdTemplatesArray);
+
+var outputPathArray = templateData.outputPath;
+// Redefine o arquivo de saída para cada módulo de saída.
+for (var t = 0; t < createdOutputModuleArray.length; t++) {
+	var o = t % outputPathArray.length;
+
+	var comp = createdTemplatesArray[t];
+	var pathIncrement = textContent(comp.layer(7)).replaceSpecialCharacters();
+	var newPath = outputPathArray[o] + '/' + pathIncrement;
+
+	createPathFolders(newPath);
+
+	var newOutputFile = new File(newPath +'/[compName].[fileextension]'); // -> PATROCINADORES FUT 2024_11-06 a 16-06
+	createdOutputModuleArray[t].file = newOutputFile;
+}
