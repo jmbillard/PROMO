56a09348d63c850ec41380807437e8967172b59d
jmbillard <jmbillard2@gmail.com>
2024-08-22 16:16:19 -0300

    -

--- ABRIDOR.js
+++ ABRIDOR.js
@@ -11,11 +11,6 @@ function readFileContent(file) {
 	return fileContent.toString();
 }
 
-var structureFile = new File('~/Downloads/structuredData.json');
-structureFile.encoding = 'UTF-8';
-var structureContent = readFileContent(structureFile);
-var structureObj = JSON.parse(structureContent);
-
 function buildComps(structureObj) {
 
 	app.beginUndoGroup('criar comps');
@@ -26,35 +21,73 @@ function buildComps(structureObj) {
 	var pageArray = [];
 	var textArray = [];
 
-	var compW = 1920;
-	var compH = 1080;
+	var compW = structureObj.pages[currentPage].width;
+	var compH = structureObj.pages[currentPage].height;
 	var compAspect = 1;
 	var compDuration = 6;
 	var compFPS = 29.97;
 
 	var comp = app.project.items.addComp('tela ' + currentPage, compW, compH, compAspect, compDuration, compFPS);
 
-	for (var i = 0; i < 10; i++) {
+	for (var i = 0; i < structureObj.elements.length; i++) {
 
 		var element = structureObj.elements[i];
 
 		if (!element.Text) continue;
 
-		var txt = element.Text;
 		var page = element.Page;
+		var bounds = element.Bounds;
 
 		if (page != currentPage) {
-
 			currentPage = page;
 			comp = app.project.items.addComp('tela ' + currentPage, compW, compH, compAspect, compDuration, compFPS);
 		}
 
-		comp.layers.addText(txt);
-	}
-	// return textArray;
-	app.endUndoGroup();
+		var currentText = comp.layers.addBoxText([bounds[2] - bounds[0], bounds[3] - bounds[1]]);
 
+		var textProp = currentText.property("ADBE Text Properties").property("ADBE Text Document");
+		var textDoc = textProp.value;
+		textDoc.resetCharStyle();
+
+		var txtProp = {
+			text: element.Text,
+			fontSize: element.TextSize,
+			justification: 7413,
+			tracking: 0
+		};
+		if (element.attributes.TextAlign == 'Center') txtProp.justification = 7415;
+		if (element.attributes.TextAlign == 'End') txtProp.justification = 7414;
+		if (element.attributes.LineHeight != undefined) txtProp.leading = element.attributes.LineHeight;
+
+		for (var p in txtProp) {
+			textDoc[p] = txtProp[p];
+			textProp.setValue(textDoc);
+		}
+
+		try {
+			textDoc.font = element.Font.name.replace(/^.+\+/, '');
+			textProp.setValue(textDoc);
+		} catch (err) {
+			textDoc.font = 'TimesNewRomanPSMT';
+			textProp.setValue(textDoc);
+		}
+
+		// transformations...
+		var transform = currentText.property('ADBE Transform Group');
+		var pos = [bounds[0] + (bounds[2] - bounds[0]) / 2, compH - bounds[1] - (bounds[3] - bounds[1]) / 2, 0];
+		transform.property('ADBE Position').setValue(pos);
+
+		currentText.moveToEnd();
+	}
+	app.endUndoGroup();
 }
 
-buildComps(structureObj);
-// alert(testArray.join('\n'));
+var structureFile = File.openDialog('selecione o arquivo de estrutura', "*.json", false);
+
+if (structureFile != null) {
+	structureFile.encoding = 'UTF-8';
+	var structureContent = readFileContent(structureFile);
+	var structureObj = JSON.parse(structureContent);
+
+	buildComps(structureObj);
+}
